877db7959367f5b387734e16b4c0db9f19201adf
Olivier Abdesselam <yazgoo@gmail.com>
2013-07-06 20:49:52 +0200

    mqtt video module working

--- module/homemodule.rb
+++ module/homemodule.rb
@@ -5,31 +5,32 @@ class MqttManaged
     Actuator = '/home/actuators'
     Host = 'localhost'
     @@modules = []
+    attr_reader :name
     def initialize
         self.class.mqtt_run if @@modules.empty?
+        @name = self.class.name
         @@modules << self
     end
     def mqtt_publish_values
-        topic = "#{Actuator}/blah/values"
-        puts topic
-        puts "lol"
-        p @actions
-        puts "publish #{topic} #{@actions.to_json}"
+        topic = "#{Actuator}/#{@name}/values"
         @@client.publish topic, @actions.to_json
-        
     end
     def self.mqtt_run
         @@client = MQTT::Client.connect(Host)
         Thread.new do
             MQTT::Client.connect(Host) do |c|
-                c.get(Actuator) do |topic,message|
+                c.get(Actuator + "/#") do |topic,message|
                     puts topic, message
                     if topic == Actuator and message == 'list'
                         @@modules.each do |mod|
                             mod.mqtt_publish_values
                         end
                     else
-                        puts "other " + topic + " " + message
+                        @@modules.each do |mod|
+                            if topic == "#{Actuator}/#{mod.name}"
+                                mod.call *message.split
+                            end
+                        end
                     end
                 end
             end
@@ -38,19 +39,18 @@ class MqttManaged
 end
 class HomeModule < MqttManaged
     def action name, parameters, _proc
-        @actions[name.to_s] = {
-            :parameters => parameters,
-            :proc => _proc
-        }
+        @procs[name.to_s] = _proc
+        @actions[name.to_s] = parameters
     end
     def initialize
         super
         @actions = {}
+        @procs = {}
         setup
     end
     def call name, *parameters
         name = name.to_s
         return if @actions[name].nil?
-        @actions[name][:proc].call(name, parameters[0])
+        @procs[name].call(name, parameters[0])
     end
 end
